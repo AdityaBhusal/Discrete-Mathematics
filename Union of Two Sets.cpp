e510b6186cf211ec0bf0898092b03fa90eace5b8
AdityaBhusal <121667136+AdityaBhusal@users.noreply.github.com>
2023-01-01 21:05:37 +0545

    Final Version

--- Union of Two Sets.cpp
+++ Union of Two Sets.cpp
@@ -1,65 +1,55 @@
-// union of two sets
 #include<iostream>
 using namespace std;
-int main()
-{
-    int nA, nB;
-    cout<<"Enter elements of set A and set B respectively : "<<endl;
-    cin>>nA>>nB;
+int main(){
+	int nA, nB, nU, flag=0;
+	cout<<"Enter number of elements for set A and set B : ";
+	cin>>nA>>nB;
+	
+	
+	nU=nA+nB;
+	
+	int setA[nA], setB[nB], setU[nU];
+	cout<<"Enter elements of set A: "<<endl;
+	for(int i = 0; i<nA; i++)
+	{
+		cin>>setA[i];
+		
+	}
+	cout<<"Enter elements of set B: "<<endl;
+	for(int j = 0; j<nB; j++)
+	{
+		cin>>setB[j];
+	}
+	int counter=0;
+	
+	cout<<"Elements of AUB are: ";
+	for (int i=0; i<nA; i++)
+	{
+		for (int j=0; j<nB; j++)
+		{
+			if(i==0)
+			{
+				setU[counter]=setB[j];
+                counter++;
+			}
+			if(setA[i]==setB[j])
+			{
+				flag++;
+			}
+		}
+		if(flag==0)
+		{
+			setU[counter]=setA[i];
+            counter++;
+		}
+			flag=0;
+		
+	}
 
-    //initializing the size of the array or set to be represented 
-    int setA[nA], setB[nB];
-  
-    //for union highest number of set would be sum of both set A and set B
-    int nU=nA+nB;
-    cout<< "The highest number of elements in Union set would be : " <<nU<<endl;
-    int setU[nU];
-    
-    //Entering the elements of set A
-    cout<<"Enter the elements of set A : "<<endl;
-     for (int i = 0; i < nA; i++)
-     {
-        cin>>setA[i];
-     }
-     
-    
-    //Entering the elements of set B
-    cout<<"Enter the elements of set B : "<<endl;
-     for (int i = 0; i < nB; i++)
-     {
-        cin>>setB[i];
-     }
-     
-     //FOR UNION
-     int k=0;
-     for (int i = 0; i < nA; i++)
-     {
-        setA[i]=setU[i];
-        k++;
-     }
-     for (int i = 0; i < nB; i++)
-     {
-        for (int j = 0; j < nA; j++)
-        {
-            if (setA[j]!=setB[i])
-            {
-                k++;
-               setU[k]=setB[i];
-               
-            }
-            
-        }
-        
-     }
-
-     //expression of union set
-
-     cout<<"The elements of union set are : ";
-     for ( int i = 0; i <=k; i++)
-     {
-        cout<<setU[i]<<endl;
-     }
-     
-     
-   //This is the change made to this file
-}
+    cout<<"The elements of Set A union Set B are : ";
+    for (int i = 0; i < counter; i++)
+    {
+        cout<<setU[i]<<" ";
+    }
+    return 0;
+	}
